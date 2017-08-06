6b6f3b32b7d21e33b57b496e6ce982d2d3132d64
Akhil Narang <akhilnarang.1999@gmail.com>
2017-08-06 20:48:01 +0200

    Stats: RR v5.8.4
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- src/android/romstats/Const.java
+++ src/android/romstats/Const.java
@@ -15,7 +15,7 @@ public class Const {
 	public static final int ROMSTATS_REPORTING_MODE_OLD = 1; // old CM10  : user prompt, default FALSE, first time immediately
 
 	public static final String STATS_URL = "https://resurrectionremix.sourceforge.io";
-	public static final String RR_VERSION = "5.8.3";
+	public static final String RR_VERSION = "5.8.4";
 	public static final String ROMNAME = "ResurrectionRemix";
 	public static final int TIMEFRAME = 1;
 }
