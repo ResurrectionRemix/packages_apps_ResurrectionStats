5e1f002b4cbb5e3b5040519cfcad87a3e4cb243d
Gabriele M <moto.falcon.git@gmail.com>
2017-11-30 23:41:00 +0100

    Updater: Android Studio support
    See README-STUDIO.txt for more info.
    
    Change-Id: Ie673bba5618b0f00a585049cd85687a64db2b420
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- Android.mk
+++ Android.mk
@@ -12,5 +12,22 @@ LOCAL_STATIC_JAVA_LIBRARIES := androidx.core_core androidx.appcompat_appcompat
 
 include $(BUILD_PACKAGE)
 
-# Use the folloing include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+LOCAL_MODULE := StudioLibs
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+resurrectionstats_system_deps := $(call java-lib-deps,framework)
+resurrectionstats_system_libs_path := $(abspath $(LOCAL_PATH))/system_libs
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+.PHONY: copy_resurrectionstats_system_deps
+copy_resurrectionstats_system_deps: $(resurrectionstats_system_deps)
+	$(hide) mkdir -p $(resurrectionstats_system_libs_path)
+	$(hide) rm -rf $(resurrectionstats_system_libs_path)/*.jar
+	$(hide) cp $(resurrectionstats_system_deps) $(resurrectionstats_system_libs_path)/framework.jar
+
+$(LOCAL_BUILT_MODULE): copy_resurrectionstats_system_deps
+	$(hide) echo "Fake: $@"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) touch $@
