9ac212e96f470604b15746d1577065e1b60fbd3e
Harsh Shandilya <msfjarvis@gmail.com>
2019-01-02 16:07:13 +0530

    Fixup Android Makefile for AOSP P
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- Android.mk
+++ Android.mk
@@ -4,11 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
+LOCAL_PRIVATE_PLATFORM_APIS := false
 LOCAL_PACKAGE_NAME := ResurrectionStats
 LOCAL_CERTIFICATE := platform
-
-LOCAL_JAVA_LIBRARIES += org.apache.http.legacy android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES := androidx.core_core androidx.appcompat_appcompat
 
 include $(BUILD_PACKAGE)
 
