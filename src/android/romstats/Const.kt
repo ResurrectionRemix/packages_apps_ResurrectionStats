07f4c6c53dcd48e52c16ae072b6794df62d7c83c
Akhil Narang <akhilnarang.1999@gmail.com>
2019-09-02 12:55:10 +0530

    ResurrectionStats: bump version
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- src/android/romstats/Const.kt
+++ src/android/romstats/Const.kt
@@ -12,7 +12,7 @@ object Const {
 
     const val STATS_URL = "https://resurrectionremix-pie.sourceforge.io/"
     const val VIEW_URL = "https://stats.resurrectionremix.com/"
-    const val RR_VERSION = "7.0.2"
+    const val RR_VERSION = "7.1.0"
     const val ROMNAME = "ResurrectionRemix"
     const val TIMEFRAME = 1
     val DEBUG = BuildConfig.DEBUG
